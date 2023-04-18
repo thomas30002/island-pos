6bc8825fa57c12c12bd336583787815803978238
dhruvilxcode <dhruvilxcode@gmail.com>
2023-04-18 18:29:50 +0530

    few styling changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Search() {
   return (
-    <button className='hover:cursor-pointer no-drag w-60 fixed top-6 right-8 flex items-center justify-between gap-2 bg-ipos-grey-50 text-ipos-grey px-4 py-4 rounded-2xl'>
+    <button className='hover:cursor-pointer no-drag w-60 flex items-center justify-between gap-2 bg-ipos-grey-50 text-ipos-grey px-4 py-4 rounded-2xl'>
       <div className='flex items-center gap-2'>
         <IconSearch />
         <p>Search</p>
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -4,8 +4,10 @@ import Search from '../components/Search.jsx'
 export default function HomePage() {
   return (
     <div>
-      <Search />
-      <h1 className='text-xl'>
+      <div className='fixed right-8 top-6'>
+        <Search />
+      </div>
+      <h1 className='text-3xl mt-20'>
         Welcome 👋
         <br />To Your Dashboard
       </h1>
