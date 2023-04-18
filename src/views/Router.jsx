f1b95b874283dae86b2487cf884b53be4a7c9e48
dhruvilxcode <dhruvilxcode@gmail.com>
2023-04-18 17:56:17 +0530

    empty pages created as per navigation.

--- src/views/Router.jsx
+++ src/views/Router.jsx
@@ -3,6 +3,13 @@ import { HashRouter, Routes, Route } from "react-router-dom"
 import { Toaster } from 'react-hot-toast';
 import Dashboard from './Dashboard.jsx';
 import HomePage from './HomePage.jsx';
+import POSPage from './POSPage.jsx';
+import ProductsPage from "./ProductsPage.jsx";
+import CustomersPage from "./CustomersPage.jsx";
+import ExpensesPage from "./ExpensesPage.jsx";
+import DiscountPage from "./DiscountPage.jsx";
+import ReportsPage from "./ReportsPage.jsx";
+import SettingsPage from "./SettingsPage.jsx";
 
 export default function RouterPage() {
   return (
@@ -12,6 +19,14 @@ export default function RouterPage() {
         <Routes>
           <Route path='/' element={<Dashboard />}>
             <Route path='' element={<HomePage/>} />
+            <Route path='pos' element={<POSPage />} />
+            <Route path='products' element={<ProductsPage />} />
+            <Route path='customers' element={<CustomersPage />} />
+            <Route path='expenses' element={<ExpensesPage />} />
+            <Route path='discount' element={<DiscountPage />} />
+            <Route path='reports' element={<ReportsPage />} />
+            <Route path='settings' element={<SettingsPage />} />
+
           </Route>
         </Routes>
       </HashRouter>
