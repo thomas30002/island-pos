254cf9294c2a2743f5076f833de664ba9413c91d
dhruvilxcode <dhruvilxcode@gmail.com>
2023-04-28 18:57:13 +0530

    dashboard design started.

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Search from '../components/Search.jsx'
+import { IconArrowDownLeft, IconArrowUpRight } from '@tabler/icons-react'
 
 export default function HomePage() {
   return (
@@ -11,6 +12,67 @@ export default function HomePage() {
         Welcome 👋
         <br />To Your Dashboard
       </h1>
+
+      <div className="mt-8 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+
+        {/* today sales */}
+        <div className='border border-ipos-grey-100 rounded-2xl px-4 py-3'>
+          <p className='text-sm'>Today Sales</p>
+
+          <div className='flex mt-3 justify-between'>
+            <p className='text-3xl font-bold'>₹10,500</p>
+            <div className='flex flex-col items-end'>
+              <div className='flex items-center text-sm font-bold gap-1'>
+                <div className='w-7 h-7 flex items-center justify-center rounded-full bg-ipos-green-50 text-ipos-green'>
+                  <IconArrowUpRight />
+                </div>
+                <p>12%</p>
+              </div>
+              <p className="text-xs">from  yesterday</p>
+            </div>
+          </div>
+        </div>
+        {/* today sales */}
+
+        {/* this week sales */}
+        <div className='border border-ipos-grey-100 rounded-2xl px-4 py-3'>
+          <p className='text-sm'>This Week Sales</p>
+
+          <div className='flex mt-3 justify-between'>
+            <p className='text-3xl font-bold'>₹40,100</p>
+            <div className='flex flex-col items-end'>
+              <div className='flex items-center text-sm font-bold gap-1'>
+                <div className='w-7 h-7 flex items-center justify-center rounded-full bg-ipos-red-50 text-ipos-red'>
+                  <IconArrowDownLeft />
+                </div>
+                <p>2%</p>
+              </div>
+              <p className="text-xs">from  last week</p>
+            </div>
+          </div>
+        </div>
+        {/* this week sales */}
+
+        {/* monthly sales */}
+        <div className='border border-ipos-grey-100 rounded-2xl px-4 py-3'>
+          <p className='text-sm'>Monthly Sales</p>
+
+          <div className='flex mt-3 justify-between'>
+            <p className='text-3xl font-bold'>₹1.5L</p>
+            <div className='flex flex-col items-end'>
+              <div className='flex items-center text-sm font-bold gap-1'>
+                <div className='w-7 h-7 flex items-center justify-center rounded-full bg-ipos-green-50 text-ipos-green'>
+                  <IconArrowUpRight />
+                </div>
+                <p>32%</p>
+              </div>
+              <p className="text-xs">from  last month</p>
+            </div>
+          </div>
+        </div>
+        {/* monthly sales */}
+
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,11 @@ module.exports = {
         'ipos-logo-color': '#7883E3',
         'ipos-grey': '#8D8D8D',
         'ipos-grey-50': '#F6F6F6',
-        'ipos-grey-100': '#E9E9E9'
-
+        'ipos-grey-100': '#E9E9E9',
+        'ipos-green-50': '#E5FFE4',
+        'ipos-green': '#39B245',
+        'ipos-red-50': '#FFE4E4',
+        'ipos-red': '#B23939'
       }
     },
   },
