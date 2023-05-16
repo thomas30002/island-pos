9b1f5d453395f0e41443dd2d1843e27328a41dc1
dhruvilxcode <dhruvilxcode@gmail.com>
2023-05-16 19:51:30 +0530

    - POS cart header, actions fixed position. - POS cart total calculated.

--- src/renderer/src/views/POSPage.jsx
+++ src/renderer/src/views/POSPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { IconClearAll, IconFilter, IconPencil, IconSearch, IconTrash } from "@tabler/icons-react";
+import { IconClearAll, IconDiscount2, IconFilter, IconPencil, IconSearch, IconTrash } from "@tabler/icons-react";
 
 import { toast } from 'react-hot-toast'
 import { CURRENCIES } from "../config/currencies.config.js";
@@ -22,6 +22,14 @@ export default function POSPage() {
   });
 
   const { products, categories, customers, cart } = state;
+
+  // cart total
+  const cartTotal = cart.reduce((pV, cV, index, arr)=>{
+    const itemTotal = cV.price * cV.quantity;
+
+    return pV+itemTotal;
+  }, 0);
+  // cart total
 
   useEffect(() => {
     _initPOS();
@@ -129,34 +137,37 @@ export default function POSPage() {
       {/* all items */}
 
       {/* cart */}
-      <div className="border rounded-2xl w-[30%] h-[95vh] p-4">
-        <div className="flex items-center justify-between">
-          <h3 className="font-bold">Cart</h3>
+      <div className="border rounded-2xl w-[30%] h-[95vh] relative overflow-y-scroll">
+        <div className="bg-white/80 backdrop-blur-sm sticky top-0 left-0 right-0 rounded-2xl px-4 py-3">
+          <div className="flex items-center justify-between">
+            <h3 className="font-bold">Cart</h3>
 
-          <button className="text-red-400">
-            <IconClearAll />
-          </button>
+            <button className="text-red-400">
+              <IconClearAll />
+            </button>
+          </div>
+          <select className="mt-4 w-full px-4 py-3 border rounded-2xl">
+              <option value="walk-in">Walk In Customer</option>
+          </select>
         </div>
 
-        <div className="mt-4">
-          <select className="w-full px-4 py-3 border rounded-2xl">
-            <option value="walk-in">Walk In Customer</option>
-          </select>
+        {
+          cart.length !== 0 ? <div className=" px-4">
 
-          <table className="w-full border-collapse mt-4">
+          <table className="w-full border-collapse">
             <thead>
-              <tr className="border-b border-b-ipos-grey-100">
-                <th className="py-3 font-normal text-left max-w-[120px]">Item</th>
-                <th className="py-3 font-normal text-left"></th>
-                <th className="py-3 font-normal text-left">Qty</th>
-                <th className="py-3 font-normal text-left">Amount</th>
+              <tr className="">
+                <th className="py-3 text-sm font-normal text-left max-w-[120px]">Item</th>
+                <th className="py-3 text-sm font-normal text-left"></th>
+                <th className="py-3 text-sm font-normal text-right">Qty</th>
+                <th className="py-3 text-sm font-normal text-right">Amount</th>
               </tr>
             </thead>
             <tbody>
               {cart.map((cartItem, index)=>{
                 return <tr key={index} className="border-b border-b-ipos-grey-100">
-                  <td className="py-3 max-w-[120px] overflow-hidden text-ellipsis whitespace-nowrap">{cartItem.name}</td>
-                  <td className="py-3 flex gap-2">
+                  <td className="text-sm py-3 max-w-[120px] overflow-hidden text-ellipsis whitespace-nowrap">{cartItem.name}</td>
+                  <td className="text-sm py-3 flex gap-2">
                     <button className="text-ipos-blue block">
                       <IconPencil />
                     </button>
@@ -166,13 +177,41 @@ export default function POSPage() {
                       <IconTrash />
                     </button>
                   </td>
-                  <td className="py-3">{cartItem.quantity}</td>
-                  <td className="py-3">{currencySymbol}{Math.round(cartItem.quantity*cartItem.price)}</td>
+                  <td className="text-sm text-right py-3">{cartItem.quantity}</td>
+                  <td className="text-sm text-right py-3">{currencySymbol}{Math.round(cartItem.quantity*cartItem.price)}</td>
                 </tr>
               })}
             </tbody>
           </table>
-        </div>
+          <div className="h-96"></div>
+        </div> : <></>
+        }
+
+        {
+          cart.length !== 0 ? <div className="bg-white/80 backdrop-blur-sm py-3 sticky bottom-0 left-0 right-0 rounded-2xl px-4">
+          <div className="flex items-center justify-between">
+            <p>Net Total</p>
+            <p className="font-bold">{currencySymbol}{cartTotal}</p>
+          </div>
+          <div className="mt-2 flex items-center justify-between">
+            <p>Discount</p>
+            <p className="font-bold text-red-400">-{currencySymbol}0</p>
+          </div>
+          <div className="mt-2 flex items-center justify-between">
+            <p>Payable Total</p>
+            <p className="font-bold">{currencySymbol}{cartTotal}</p>
+          </div>
+          <div className="mt-4 flex flex-col gap-2">
+            <button className="px-4 py-3 rounded-2xl bg-ipos-grey-100 text-ipos-grey hover:bg-ipos-grey-50 flex justify-center gap-2">
+              <IconDiscount2 />
+              Apply Discount
+            </button>
+            <button className="px-4 py-3 rounded-2xl bg-ipos-blue hover:bg-ipos-logo-color text-white block">
+              Checkout
+            </button>
+          </div>
+        </div> : <></>
+        }
       </div>
       {/* cart */}
     </div>
