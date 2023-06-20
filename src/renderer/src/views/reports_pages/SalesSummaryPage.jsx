10d6970c7094f63676c149aeaf0d25918fec4f9a
dhruvilxcode <dhruvilxcode@gmail.com>
2023-06-20 18:40:30 +0530

    dummy data chart added in sales summary reports.

--- src/renderer/src/views/reports_pages/SalesSummaryPage.jsx
+++ src/renderer/src/views/reports_pages/SalesSummaryPage.jsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+  Legend
+);
 
 export default function SalesSummaryPage() {
 
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+        display: false,
+      },
+      title: {
+        display: false,
+        text: 'Reports',
+      },
+    },
+  };
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const data = {
+    labels,
+    datasets: [
+      {
+        fill: true,
+        label: 'Sales',
+        data: [20000, 24000, 12000, 15000, 30000, 23000, 50000],
+        // borderColor: 'rgb(53, 162, 235)',
+        // backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+    ],
+  };
 
   return (
     <div className='px-8 py-6 w-full'>
@@ -20,8 +69,8 @@ export default function SalesSummaryPage() {
       </div>
 
       <div className="mt-8">
-        <div className="w-full h-96">
-          
+        <div className="w-full p-4 border rounded-2xl">
+          <Line options={options} data={data} className='w-full' />
         </div>
       </div>
     </div>
